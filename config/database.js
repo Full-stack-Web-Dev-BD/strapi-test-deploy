b9bec4c3672d7dffa6c8db9596f14facd5188247
Alamin <alaminprogramerr@gmail.com>
2021-06-18 20:07:24 +0600

    init

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const pars = require("pg-connection-string").parse;
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    const config = pars(process.env.DATABASE_URL);
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '77855912-b32a-429f-8569-2ad7eded4bd5'
+};
